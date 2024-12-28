f5109b66c4db912b729d43d5fd4f0f67d3b8d72a
Smith-S-S <80092760+Smith-S-S@users.noreply.github.com>
2024-12-28 13:55:24 +0800

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -1,12 +1,11 @@
 from pyngrok import ngrok
 import subprocess
-import os
 
 # Set your ngrok auth token
 ngrok.set_auth_token("2qpRWeIX1oVNuMoOacmRWFXqyXQ_2ZDQHJLZK5FyB8iXJpe23")
 
 # Start ngrok tunnel on port 8501 (Streamlit's default port)
-public_url = ngrok.connect(port=8501)
+public_url = ngrok.connect("8501", "http")  # Correct configuration for ngrok
 
 # Print the public URL so it can be captured by the GitHub Actions workflow
 print(f"Public URL: {public_url}")
